92327880911d2b6f735459986d5495d9ad636865
a2c6201 <a2c6201@gmail.com>
2025-01-05 15:46:14 +0900

    chore:lintツールからのline too long指摘を修正

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
     response = {
@@ -22,44 +23,97 @@ async def root():
             [
                 {
                     "article_id": "9834f9190fad7132",
-                    "title": "「SoftBankウインターカップ2024」男子・九州学院(熊本)2回戦へ(KAB熊本朝日放送)",
-                    "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
-                    "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
+                    "title": (
+                        "「SoftBankウインターカップ2024」男子・九州学院(熊本)2回戦へ(KAB熊本朝日放送)"
+                        ),
+                    "link": (
+                        "https://news.yahoo.co.jp/articles/"
+                        "60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss"
+                        ),
+                    "image_url": (
+                        "https://newsatcl-pctr.c.yimg.jp"
+                        "/t/amd-img/20241224-00010000-kab-000-1-view.jpg?"
+                        "exp=10800&h=253&pri=l&w=450"
+                        ),
                     "category": ["domestic"]
                 },
                 {
                     "article_id": "9834f9190fad7132",
-                    "title": "吉田沙保里、スカイツリーのさきっぽへし折る",
-                    "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
-                    "image_url": "https://image.lgtmoon.dev/317256",
+                    "title": (
+                        "吉田沙保里、スカイツリーのさきっぽへし折る"
+                    ),
+                    "link": (
+                        "https://news.yahoo.co.jp/articles/"
+                        "60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss"
+                    ),
+                    "image_url": (
+                        "https://image.lgtmoon.dev/317256"
+                    ),
                     "category": ["domestic"]
                 },
                 {
                     "article_id": "00001",
-                    "title": "「タイムカプセルを探していただけなんです」学校の地面からウラン湧き出る、生徒が泥酔状態に",
-                    "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
-                    "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
+                    "title": (
+                        "「タイムカプセルを探していただけなんです」"
+                        "学校の地面からウラン湧き出る、生徒が泥酔状態に"
+                    ),
+                    "link": (
+                        "https://news.yahoo.co.jp/articles/"
+                        "60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss"
+                    ),
+                    "image_url": (
+                        "https://newsatcl-pctr.c.yimg.jp/t/amd-img/"
+                        "20241224-00010000-kab-000-1-view.jpg"
+                        "?exp=10800&h=253&pri=l&w=450"
+                    ),
                     "category": ["domestic"]
                 },
                 {
                     "article_id": "00002",
-                    "title": "真木よう子大量発生 - ティンコンカンコン警備隊発足",
-                    "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
-                    "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
+                    "title": (
+                        "真木よう子大量発生 - ティンコンカンコン警備隊発足"
+                    ),
+                    "link": (
+                        "https://news.yahoo.co.jp/articles/"
+                        "60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss"
+                    ),
+                    "image_url": (
+                        "https://newsatcl-pctr.c.yimg.jp/t/amd-img/"
+                        "20241224-00010000-kab-000-1-view.jpg?"
+                        "exp=10800&h=253&pri=l&w=450"
+                    ),
                     "category": ["domestic"]
                 },
                 {
                     "article_id": "00003",
-                    "title": "K-POPアイドル、軽POPアイドルに",
-                    "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
-                    "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
+                    "title": (
+                        "K-POPアイドル、軽POPアイドルに"
+                    ),
+                    "link": (
+                        "https://news.yahoo.co.jp/articles/"
+                        "60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss"
+                    ),
+                    "image_url": (
+                        "https://newsatcl-pctr.c.yimg.jp/t/amd-img/"
+                        "20241224-00010000-kab-000-1-view.jpg?"
+                        "exp=10800&h=253&pri=l&w=450"
+                    ),
                     "category": ["domestic"]
                 },
                 {
                     "article_id": "00004",
-                    "title": "「こんなはずじゃなかった」あばれる君破産",
-                    "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
-                    "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
+                    "title": (
+                        "「こんなはずじゃなかった」あばれる君破産"
+                    ),
+                    "link": (
+                        "https://news.yahoo.co.jp/articles/"
+                        "60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss"
+                    ),
+                    "image_url": (
+                        "https://newsatcl-pctr.c.yimg.jp/t/amd-img/"
+                        "20241224-00010000-kab-000-1-view.jpg?"
+                        "exp=10800&h=253&pri=l&w=450"
+                    ),
                     "category": ["domestic"]
                 }
             ]
