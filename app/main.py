985f5d7c3cb7a81c24ea2ee385b0f5744c460505
a2c6201 <a2c6201@gmail.com>
2025-01-05 15:38:35 +0900

    chore:固有のarticle_id設定

--- app/main.py
+++ app/main.py
@@ -35,28 +35,28 @@ async def root():
                     "category": ["domestic"]
                 },
                 {
-                    "article_id": "9834f9190fad7132",
+                    "article_id": "00001",
                     "title": "「タイムカプセルを探していただけなんです」学校の地面からウラン湧き出る、生徒が泥酔状態に",
                     "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
                     "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
                     "category": ["domestic"]
                 },
                 {
-                    "article_id": "9834f9190fad7132",
+                    "article_id": "00002",
                     "title": "真木よう子大量発生 - ティンコンカンコン警備隊発足",
                     "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
                     "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
                     "category": ["domestic"]
                 },
                 {
-                    "article_id": "9834f9190fad7132",
+                    "article_id": "00003",
                     "title": "K-POPアイドル、軽POPアイドルに",
                     "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
                     "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
                     "category": ["domestic"]
                 },
                 {
-                    "article_id": "9834f9190fad7132",
+                    "article_id": "00004",
                     "title": "「こんなはずじゃなかった」あばれる君破産",
                     "link": "https://news.yahoo.co.jp/articles/60d07f8f3c32349dfec9216394cad299544b1c9a?source=rss",
                     "image_url": "https://newsatcl-pctr.c.yimg.jp/t/amd-img/20241224-00010000-kab-000-1-view.jpg?exp=10800&h=253&pri=l&w=450",
